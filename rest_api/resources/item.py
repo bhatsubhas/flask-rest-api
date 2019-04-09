3033ef81ce9465403e01b2c3c68b7135375f0dcb
Subhas Bhat <subhas.bhat@subex.com>
2019-04-09 08:50:13 +0530

    Add store_id as mandatory in ItemModel
    Since store_id is made mandatory. To get or delete any item, store_id has to be
    passed as a parameter

--- rest_api/resources/item.py
+++ rest_api/resources/item.py
@@ -4,41 +4,40 @@ from rest_api.models.item import ItemModel
 
 
 class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-                        type=float,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
+    parser1 = reqparse.RequestParser()
+    parser1.add_argument('price',
+                         type=float,
+                         required=True,
+                         help="This field cannot be left blank!"
+                         )
+    parser1.add_argument('store_id',
+                         type=int,
+                         required=True,
+                         help="Every item needs a store_id"
+                         )
 
-    parser.add_argument('store_id',
-                        type=int,
-                        required=True,
-                        help="Every item needs a store_id"
-                        )
-    get_parser = reqparse.RequestParser()
-    get_parser.add_argument('store_id',
-                            type=int,
-                            required=True,
-                            help="store_id is mandatory"
-                            )
+    parser2 = reqparse.RequestParser()
+    parser2.add_argument('store_id',
+                         type=int,
+                         required=True,
+                         help="store_id is mandatory"
+                         )
 
     @jwt_required()
     def get(self, name):
-        data = Item.get_parser.parse_args()
-        store_id = data.get('store_id',None)
+        data = Item.parser2.parse_args()
+        store_id = data['store_id']
         item = ItemModel.find_by_name(name, store_id)
         if item:
             return item.json(), 200
-        return {'message': 'Item not found'}, 404
+        return {'message': 'Item not found in store with store_id {}'.format(store_id)}, 404
 
     @jwt_required()
     def post(self, name):
-        data = Item.parser.parse_args()
+        data = Item.parser1.parse_args()
         if ItemModel.find_by_name(name, data['store_id']):
             return {'message': "An item with name '{}' already exists".format(name)}, 400
 
-        data = Item.parser.parse_args()
         item = ItemModel(name, **data)
         try:
             item.save_to_db()
@@ -49,7 +48,7 @@ class Item(Resource):
 
     @jwt_required()
     def put(self, name):
-        data = Item.parser.parse_args()
+        data = Item.parser1.parse_args()
 
         item = ItemModel.find_by_name(name, data['store_id'])
         if item is None:
@@ -66,14 +65,16 @@ class Item(Resource):
 
     @jwt_required()
     def delete(self, name):
-        item = ItemModel.find_by_name(name)
+        data = Item.parser2.parse_args()
+        store_id = data['store_id']
+        item = ItemModel.find_by_name(name, store_id)
         if item:
             item.delete_from_db()
             return {'message': 'Item deleted'}, 200
-        return {'message': "Item with name '{}' does not exist".format(name)}, 400
+        return {'message': "Item with name '{}' and store_id {} does not exist".format(name, store_id)}, 400
 
 
 class ItemList(Resource):
     @jwt_required()
     def get(self):
-        return {'items': [item.json() for item in ItemModel.query.all()]}, 200
+        return {'items': [_.json() for _ in ItemModel.query.all()]}, 200
